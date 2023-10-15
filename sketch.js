a02ff1f30ff1cc0b34285fbc5feccf3e26311105
xxhyz233 <49100459+xxhyz233@users.noreply.github.com>
2023-10-14 21:02:17 -0400

    1st Push

--- sketch.js
+++ sketch.js
@@ -1,13 +1,103 @@
+let xPos = 400;
+let yPos = 400;
+let ySpeed = 0;
+let Accel = 0.5;
+
+let diameter = 50;
+let rWidth = 50;
+let rHeight = 50;
+
+class Physical
+{
+    constructor(xPos, yPos, diameter)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.diameter = diameter;
+        this.ySpeed = ySpeed;
+    }
+
+    drop()
+    {
+        this.ySpeed = this.ySpeed + Accel;
+
+        this.yPos = this.yPos + this.ySpeed;
+
+        if (this.yPos + this.diameter/2 > height)
+        {
+            this.ySpeed = -this.ySpeed;
+            this.yPos = height - this.diameter/2;
+        }
+
+        this.ySpeed = this.ySpeed * 0.997;
+    }
+}
+
+class PhysEllipse extends Physical
+{
+    constructor(xPos, yPos, diameter)
+    {
+        super(xPos,yPos);
+        this.diameter = diameter;
+    }
+
+    drop()
+    {
+        this.ySpeed = this.ySpeed + Accel;
+
+        this.yPos = this.yPos + this.ySpeed;
+
+        if (this.yPos + this.diameter/2 > height)
+        {
+            this.ySpeed = -this.ySpeed;
+            this.yPos = height - this.diameter/2;
+        }
+
+        this.ySpeed = this.ySpeed * 0.997;
+        ellipse(this.xPos, this.yPos, this.diameter);
+    }
+}
+
+class PhysRect extends Physical
+{
+    constructor (xPos, yPos, rWidth, rHeight)
+    {
+        super(xPos, yPos);
+        this.rWidth = rWidth;
+        this.rHeight = rHeight;
+    }
+
+    drop()
+    {
+        this.ySpeed = this.ySpeed + Accel;
+
+        this.yPos = this.yPos + this.ySpeed;
+
+        if ((this.yPos + this.rWidth > width) || (this.yPos + this.rHeight > height))
+        {
+            this.ySpeed = -this.ySpeed;
+            this.yPos = height - this.rHeight;
+        }
+
+        this.ySpeed = this.ySpeed * 0.997;
+        rect(this.xPos, this.yPos, this.rWidth, this.rHeight);
+    }
+}
+
+let ball1 = new PhysEllipse(400,400,60);
+let rect1 = new PhysRect(400,400,80,80);
+
 function setup()
 {
     createCanvas(800,800);
-    background(0);
 }
 
 function draw()
 {
     background(0);
-    fill(255,0,0);
-    noStroke();
-    ellipse(mouseX, mouseY, 50, 50);
+    fill(200,0,0);
+    ball1.drop();
+    rect1.drop();
+    
 }
+
