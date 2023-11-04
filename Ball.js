5ff00ae2ec598e0f28c221649513133d585c1f71
xxhyz233 <49100459+xxhyz233@users.noreply.github.com>
2023-11-03 22:52:29 -0400

    Added collisions between the balls and minor tweaks

--- Ball.js
+++ Ball.js
@@ -1,6 +1,11 @@
 // rule defines how the physics work (Sticky, Bouncy etc...)
 let rule = 0;
 let airRes = 0.997;
+let diameter = 50;
+let radius = diameter/2;
+// isEdge detects whether the ball touches the edge or not
+let isEdge = false;
+
 class Ball
 {
   constructor(x,y, ax, ay, r)
@@ -10,6 +15,7 @@ class Ball
     this.acceleration = new createVector(ax, ay);
     this.mass = 1;
     this.rule = r;
+    this.isEdge = false;
   }
 
   setRule(r)
@@ -21,9 +27,8 @@ class Ball
   {
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
-    this.velocity.limit(100);
-    // Use v1.limit(max) to limit the velocity
-
+    // Limits the velocity to 100
+    this.velocity.limit(50);
   }
 
   applyForce(force)
@@ -31,7 +36,18 @@ class Ball
     // Static Vector that creates a new one based on .div method
     // Non-static methods only update the current vector, not create a new one
     let f = p5.Vector.div(force, this.mass);
-    this.acceleration.add(f);
+    // If bouncy, add acceleration by force
+    if(this.rule === 0)
+    {
+      this.acceleration.mult(0);
+      this.acceleration.add(f);
+    }
+    // If sticky, set acceleration to 0
+    else if(this.rule === 1)
+    {
+      this.acceleration.x = 0;
+      this.acceleration.y = 0;
+    }
   }
   display()
   {
@@ -41,60 +57,106 @@ class Ball
     ellipse(this.position.x, this.position.y, 50, 50);
   }
 
-  checkEdges()
+  bounceEdges()
   {
+    // Rule 0: Bouncy
     if(this.rule === 0)
     {
-      if(this.position.x > width)
+      if(this.position.x > width-radius)
       {
-        this.position.x = width;
+        this.position.x = width-radius;
         this.velocity.x *= -1;
       }
-      if(this.position.x < 0)
+      if(this.position.x < radius)
       {
-          this.position.x = 0;
+        this.position.x = radius;
         this.velocity.x *= -1;
       }
-      if(this.position.y > height)
+      if(this.position.y > height-radius)
       {
-        this.position.y = height;
+        this.position.y = height-radius;
         this.velocity.y *= -1;
       }
-      if(this.position.y < 0)
+      if(this.position.y < radius)
       {
-        this.position.y = 0;
+        this.position.y = radius;
         this.velocity.y *= -1;
       }
     }
+    //!! For the rubbery effect to work, we have to communicate between border and the ball meeting the edge
 
+    // Rule 1: Sticky
     if(this.rule === 1)
     {
-      if(this.position.x > width)
+      // All the if and else if statements check if the ball goes out of the border, then sticks the ball entirely.
+      if(this.position.x > width-radius/2)
       {
-        this.position.x = width;
-        this.velocity.x *= 0;
+        // Offsets position so it doesn't go overbound
+        this.position.x = width-radius/2;
+        this.position.y = this.position.y;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+        this.acceleration.x = 0;
+        this.acceleration.x = 0;
+        // Sets isEdge to true so sketch knows when to draw other stuff
+        this.isEdge = true;
       }
-      if(this.position.x < 0)
+      else if(this.position.x < radius/2)
       {
-          this.position.x = 0;
-        this.velocity.x *= 0;
+        this.position.x = radius/2;
+        this.position.y = this.position.y;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+        this.acceleration.x = 0;
+        this.acceleration.y = 0;
+        this.isEdge = true;
       }
-      if(this.position.y > height)
+      else if(this.position.y > height-radius/2)
       {
-        this.position.y = height;
-        this.velocity.y *= 0;
+        this.position.y = height-radius/2;
+        this.position.x = this.position.x;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+        this.acceleration.x = 0;
+        this.acceleration.y = 0;
+        this.isEdge = true;
       }
-      if(this.position.y < 0)
+      else if(this.position.y < radius/2)
       {
-        this.position.y = 0;
-        this.velocity.y *= 0;
+        this.position.y = radius/2;
+        this.position.x = this.position.x;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+        this.acceleration.x = 0;
+        this.acceleration.y = 0;
+        this.isEdge = true;
+      }
+      else
+      {
+        this.isEdge = false;
       }
     }
-
   }
 
-  checkCollision()
+  //!! Add collision between the balls
+  checkCollision(other)
   {
+    let distance = p5.Vector.sub(other.position, this.position);
+    
+    // Calculate magnitude of the vector separating the balls
+    let distanceMag = distance.mag();
 
+    // Minimum distance between the balls before they touch, added 15 so
+    // it's less likely to get stuck together
+    let minDistance = diameter+15;
+    // If the distance between the two balls is shorter than the minimum distance
+    // The balls go to different directions
+    if (distanceMag < minDistance)
+    {
+      print('Time for collision');
+      this.velocity.x *= -1;
+      this.velocity.y *= -1;
+      this.velocity.limit(50);
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,42 +1,93 @@
+// The logic behind is that you could "stick" the balls by pressing R, then unstick them by releasing a force with F
 let b1;
 let b2;
 let b3;
 let balls;
 let ruleIndex;
+let drawDetect;
 
 function setup()
 {
-  createCanvas(800,800);
+  createCanvas(600,600);
+  // Balls for initialization
   b1 = new Ball(400,400, -0.001, 0.01, 0);
   b2 = new Ball(0,0, .001, .01, 0);
   b3 = new Ball(200,200, .002, -.01, 0);
   b4 = new Ball(500,500, -0.001, 0.03, 0);
-  // Array of balls
+  // ruleIndex dictates whether the balls are bouncy or sticky
+  ruleIndex = 0;
+  // Array of balls so you can add more in the future
   balls = [b1, b2, b3, b4];
+  drawDetect = false;
 }
 
 function draw()
 {
-  background(20);
+  // Color switch for rules
+  if (ruleIndex === 0)
+  {
+    background(100);
+  }
+  else if (ruleIndex === 1){
+    background (150,100,100);
+  }
+  
   // Loops through the entire array of balls, displaying and updating
   // every single one of the balls
+  push();
+  stroke(0);
+  strokeWeight(10);
+  noFill();
   for(let i = 0; i < balls.length; i++)
   {
     balls[i].update();
-    balls[i].checkEdges();
+    balls[i].bounceEdges();
     balls[i].display();
+    balls[i].setRule(ruleIndex);
+    if(balls[i].isEdge)
+    {
+      print('Is Touching Edges');
+      for (let j = 0; j < 10; j++)
+      {
+        ellipse(balls[i].position.x, balls[i].position.y, diameter+5*j);
+      }
+    }
+    // Checks for collision with every single ball in the array except for itself
+    for(let j = 0; j < balls.length ; j++)
+    {
+      
+      if(i!=j)
+      {
+        balls[i].checkCollision(balls[j]);
+      }
+    }
   }
+  pop();
+
+  // Instructions
+  push()
+  let s = 'Press F to move the balls. \nonce they have enough momentum, \n Press R to ***** them. \nMouse Press to add new balls.';
+  textSize(18);
+  textAlign(CENTER, CENTER);
+  fill(220);
+  noStroke();
+  text(s, 300, 300);
+  pop();
 }
 
 
 function mousePressed()
 {
   // Generates four balls when mouse is pressed
-  for (let i = 0; i < 4; i++)
+  if(balls.length < 10)
   {
-    // Diagonal pattern, random velocity x and y, with rule 0
-    balls.push(new Ball(mouseX+i*50, mouseY+i*50, random(-0.002, 0.002), random(-0.002, 0.002), 0));
+    for (let i = 0; i < 2; i++)
+    {
+      // Diagonal pattern, random acceleration x and y, with rule 0
+      balls.push(new Ball(mouseX+i*50, mouseY+i*50, random(-0.01, 0.01), random(-0.01, 0.01), 0));
+    }
   }
+
 }
 
 
@@ -46,31 +97,24 @@ function keyPressed()
   // Applys a random force when pressing the key f
   if (key === 'f')
   {
-    print('F Pressed');
-    tempForce = p5.Vector.random2D(1,1);
-    b1.applyForce(tempForce);
+    // Apply a random force for every ball
+    for(let i = 0; i < balls.length; i++)
+    {
+      balls[i].applyForce(p5.Vector.random2D(1,1));
+    }
+    
   }
 
   // Changes the rule when pressing the key r
-  else if (key === 'r')
+  if (key === 'r')
   {
     if(ruleIndex < 1)
     {
       ruleIndex++;
-      // Make into loop
-      b1.setRule(ruleIndex);
-      b2.setRule(ruleIndex);
-      b3.setRule(ruleIndex);
-      b4.setRule(ruleIndex);
     }
     else
     {
       ruleIndex = 0;
-      // Make into loop
-      b1.setRule(ruleIndex);
-      b2.setRule(ruleIndex);
-      b3.setRule(ruleIndex);
-      b4.setRule(ruleIndex);
     }
   }
 }
